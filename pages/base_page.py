ad572b88e87434745e9f035d16989714f63291d9
Vitaly S <vitaliksew@gmail.com>
2023-05-24 17:55:23 -0400

    delete: double function

--- pages/base_page.py
+++ pages/base_page.py
@@ -21,9 +21,6 @@ class BasePage:
     def elements_are_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
-    def element_is_visible(self, locator, timeout=5):
-        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
-
     def element_is_not_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
